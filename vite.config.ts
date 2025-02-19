256e75e7bb2468fc85ef4efad75d9b7ac8f72aff
AHuyHarryIT <ahgamingofficial03@gmail.com>
2025-02-19 17:44:16 +0700

    add dependencies and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@stores': path.resolve(__dirname, 'src/stores'),
+      '@partials': path.resolve(__dirname, 'src/partials'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
