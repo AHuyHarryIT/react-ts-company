5789983bea58751fcd9a10680c2ea42cadda117d
AHuyHarryIT <ahgamingofficial03@gmail.com>
2025-04-16 23:12:27 +0700

    build: vite config paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import tailwindcss from '@tailwindcss/vite';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
 import { defineConfig } from 'vite';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 const BASE_URL = process.env.VITE_BASE_API_URL;
 
@@ -12,24 +12,15 @@ export default defineConfig({
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     tailwindcss(),
     react(),
+    // add others plugin before this line ^
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json']
+    })
   ],
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@layouts': path.resolve(__dirname, 'src/layouts'),
-      '@services': path.resolve(__dirname, 'src/services'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@stores': path.resolve(__dirname, 'src/stores'),
-      '@partials': path.resolve(__dirname, 'src/partials'),
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
   server: {
     proxy: {
       '/api': BASE_URL || 'http://localhost:8000',
-      '/storage': BASE_URL || 'http://localhost:8000',
-    },
-  },
+      '/storage': BASE_URL || 'http://localhost:8000'
+    }
+  }
 });
