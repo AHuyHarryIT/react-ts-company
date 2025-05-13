21afc213020e3a984dff0306571e6fc5d4ee4351
AHuyHarryIT <ahgamingofficial03@gmail.com>
2025-05-13 21:48:52 +0700

    style: add additional icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,3 +5,11 @@ export {
   FaFileExport as IconExport,
   FaFilter as IconFilter
 } from 'react-icons/fa6';
+
+export { BiSearch as IconSearch } from 'react-icons/bi';
+export { LuEye as IconView } from 'react-icons/lu';
+
+export {
+  IoArrowUndo as IconRestore,
+  IoReload as IconReload
+} from 'react-icons/io5';
