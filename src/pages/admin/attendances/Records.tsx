22f1b8a7abffc9921a850f252ddea97f13d3b1a9
AHuyHarryIT <ahgamingofficial03@gmail.com>
2025-07-07 22:47:06 +0700

    refactor: remove unused React import in Records component

--- src/pages/admin/attendances/Records.tsx
+++ src/pages/admin/attendances/Records.tsx
@@ -1,5 +1,4 @@
 import ComponentCard from '@components/common/ComponentCard';
-import React from 'react';
 
 export const Records = () => {
   return <ComponentCard title="Bảng tính công">Records</ComponentCard>;
