0e578ea2013a9e3a079aa3a32a24467c0bc90187
AHuyHarryIT <ahgamingofficial03@gmail.com>
2025-08-29 18:17:07 +0700

    fix(PrintBoxStamp): remove redundant background color class from print layout

--- src/components/print/PrintBoxStamp.tsx
+++ src/components/print/PrintBoxStamp.tsx
@@ -130,7 +130,7 @@ export const PrintBoxStamp = ({
             (_, pageIndex) => (
               <div
                 key={`page-${pageIndex}`}
-                className="grid grid-cols-3 grid-rows-2 place-items-center gap-6 bg-red-500 not-print:mb-8 not-print:border not-print:border-green-500 print:h-screen print:w-full print:break-after-page"
+                className="grid grid-cols-3 grid-rows-2 place-items-center gap-6 not-print:mb-8 not-print:border not-print:border-green-500 print:h-screen print:w-full print:break-after-page"
               >
                 {Array.from(
                   {
