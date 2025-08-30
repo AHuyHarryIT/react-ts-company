20c88681578c4d16c69f81cf1c309c24494f54b9
AHuyHarryIT <ahgamingofficial03@gmail.com>
2025-08-30 08:51:26 +0700

    style: adjust font size in PrintBagStamp component for better readability

--- src/components/print/PrintBagStamp.tsx
+++ src/components/print/PrintBagStamp.tsx
@@ -146,7 +146,7 @@ export const PrintBagStamp = ({
             .map((page, pageIndex) => (
               <div
                 key={`page-${pageIndex}`}
-                className="print-grid print:page-break-after-always grid grid-cols-2 grid-rows-4 gap-4 not-print:mb-8 not-print:max-w-7xl not-print:grid-cols-1 not-print:border not-print:border-green-400 not-print:p-4 not-print:lg:grid-cols-2 print:min-h-screen"
+                className="print-grid print:page-break-after-always mr-2 grid grid-cols-2 grid-rows-4 gap-4 not-print:mb-8 not-print:max-w-7xl not-print:grid-cols-1 not-print:border not-print:border-green-400 not-print:p-4 not-print:lg:grid-cols-2 print:min-h-screen"
               >
                 {page.map(({ item, index }) => (
                   <div
@@ -163,7 +163,7 @@ export const PrintBagStamp = ({
                       </colgroup>
                       <tbody>
                         <tr>
-                          <td className="text-start text-[7.3px]">
+                          <td className="text-start text-[7.2px]">
                             Tên sản phẩm
                             <br />
                             品名
@@ -175,7 +175,7 @@ export const PrintBagStamp = ({
                           <td className="text-sm font-bold">{item.code}</td>
                         </tr>
                         <tr>
-                          <td className="text-start text-[7.3px]">
+                          <td className="text-start text-[7.2px]">
                             Nguyên liệu
                             <br />
                             原材料
@@ -183,14 +183,14 @@ export const PrintBagStamp = ({
                           <td colSpan={2} className="text-sm">
                             {product.material}
                           </td>
-                          <td className="text-[7.3px]">
+                          <td className="text-[7.2px]">
                             Màu sắc
                             <br />色
                           </td>
                           <td className="text-sm">{product.color}</td>
                         </tr>
                         <tr>
-                          <td className="text-start text-[7.3px]">
+                          <td className="text-start text-[7.2px]">
                             Số lượng
                             <br />
                             数量
@@ -200,7 +200,7 @@ export const PrintBagStamp = ({
                           </td>
                         </tr>
                         <tr>
-                          <td className="text-start text-[7.3px]">
+                          <td className="text-start text-[7.2px]">
                             Lotno
                             <br />
                             ロット No
@@ -225,24 +225,24 @@ export const PrintBagStamp = ({
                           </td>
                         </tr>
                         <tr>
-                          <td className="text-start text-[7.3px]">
+                          <td className="text-start text-[7.2px]">
                             Kiểm tra
                             <br />
                             検査
                           </td>
-                          <td colSpan={2} className="text-[7.3px]">
+                          <td colSpan={2} className="text-[7.2px]">
                             Kiểm tra 100%
                             <br />
                             檢查(100%)
                           </td>
-                          <td colSpan={2} className="text-[7.3px]">
+                          <td colSpan={2} className="text-[7.2px]">
                             Kiểm tra 200%
                             <br />
                             檢查(200%)
                           </td>
                         </tr>
                         <tr className="h-18">
-                          <td className="text-start text-[7.3px]">
+                          <td className="text-start text-[7.2px]">
                             Mộc
                             <br />
                             合格印
@@ -251,7 +251,7 @@ export const PrintBagStamp = ({
                           <td colSpan={2}></td>
                         </tr>
                         <tr>
-                          <td className="text-start text-[7.3px]">
+                          <td className="text-start text-[7.2px]">
                             Người kiểm
                             <br />
                             検査
@@ -260,10 +260,10 @@ export const PrintBagStamp = ({
                           <td colSpan={2}></td>
                         </tr>
                         <tr>
-                          <td colSpan={3} className="text-center text-[7.3px]">
+                          <td colSpan={3} className="text-center text-[7.2px]">
                             Thời gian 時間
                           </td>
-                          <td colSpan={2} className="text-[7.3px]">
+                          <td colSpan={2} className="text-[7.2px]">
                             {date.format('DD/MM/YYYY')}{' '}
                             {shift == 1 ? '07:30' : '19:30'}
                           </td>
